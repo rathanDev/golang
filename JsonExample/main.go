574baed3720407ff38404eab35b447ac954d0644
jana <rathan.kparam@gmail.com>
2022-07-16 11:16:21 +0800

    Add json unmarshal exmple

---
+++ JsonExample/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Bird struct {
+	Species     string
+	Description string
+}
+
+func main() {
+	birdJson := `{"species": "pigeon","description": "likes to perch on rocks"}`
+	var bird Bird
+	json.Unmarshal([]byte(birdJson), &bird)
+	fmt.Printf("Species: %s, Description: %s", bird.Species, bird.Description)
+}
