a52921524c7068ba83c6b083c3bb38a9041a2fae
jana <rathan.kparam@gmail.com>
2022-07-16 09:00:59 +0800

    Add helper file

--- BasicApiClient/main.go
+++ BasicApiClient/main.go
@@ -8,22 +8,6 @@ import (
     "os"
 )
 
-const baseUrl string = "http://localhost:8080"
-var version string = "/v1";
-const accountUrl string = "/organisation/accounts"
-
-func getUrl() string {
-
-	version = "/v1"
-
-	fmt.Printf("BaseUrl is %v", baseUrl)
-
-	var url = baseUrl + version + accountUrl
-	fmt.Println(url)
-
-	return url
-}
-
 func main() {
 
     url := getUrl()
