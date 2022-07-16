7060978bd4a924a5614cebdfa55286ce37de556d
jana <rathan.kparam@gmail.com>
2022-07-16 10:56:16 +0800

    Format code

--- BasicApiClient/main.go
+++ BasicApiClient/main.go
@@ -1,36 +1,35 @@
 package main
 
 import (
-    "fmt"
-    "io/ioutil"
-    "log"
-    "net/http"
-    "os"
-    "api-client/helper"
-    "api-client/model"
+	"api-client/helper"
+	"api-client/model"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
 )
 
 func main() {
 
-    url := helper.GetUrl()
+	url := helper.GetUrl()
 
-    response, err := http.Get(url)
+	response, err := http.Get(url)
 
-    if err != nil {
-        fmt.Print(err.Error())
-        os.Exit(1)
-    }
+	if err != nil {
+		fmt.Print(err.Error())
+		os.Exit(1)
+	}
 
-    responseData, err := ioutil.ReadAll(response.Body)
-    if err != nil {
-        log.Fatal(err)
-    }
-    fmt.Println(string(responseData))
+	responseData, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(responseData))
 
-    fmt.Println("")
+	fmt.Println("")
 
-    accountData := model.AccountData {ID : "some-id", OrganisationID: "org-id"}
-    fmt.Println(accountData)
-
+	accountData := model.AccountData{ID: "someId", OrganisationID: "orgId"}
+	fmt.Println(accountData)
 
 }
