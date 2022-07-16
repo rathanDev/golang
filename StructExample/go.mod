3f0100f9d8522cc7b49c0b13eff37c9c65888259
jana <rathan.kparam@gmail.com>
2022-07-16 10:45:10 +0800

    Access the struct from another package

---
+++ StructExample/go.mod
@@ -0,0 +1,3 @@
+module structs-example
+
+go 1.17
--- StructExample/structs.go
+++ StructExample/structs.go
@@ -1,22 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"structs-example/model"
+)
+
 
 type Person struct {
 	name string
 	age  int
 }
 
-func newPerson(name string) *Person {
-	p := Person{name: name}
-	p.age = 31
-	return &p
-}
-
 func main() {
-
 	p := Person{"Jana", 31}
-
 	fmt.Println(p)
 
+	// type Account a
+
+	a := model.Account{"id1", "name1"}
+	fmt.Println(a)
 }
