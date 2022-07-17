b58670cff1fe7fdccad5359c703bb0bea79f41ca
jana <rathan.kparam@gmail.com>
2022-07-17 09:55:21 +0800

    Refactor

--- BasicApiClient/operation/fetch.go
+++ BasicApiClient/operation/fetch.go
@@ -42,16 +42,11 @@ func Fetch() {
 
 	fmt.Println()
 
-	// var accounts []AccountData
 	accounts := apiResponse.Accounts
 	fmt.Println("\n")
 	fmt.Println("accounts => ", accounts)
 
-
-	// for id, organisation_id := range accounts {
 	for i, val := range apiResponse.Accounts {
-		// Each value is an interface{} type, that is type asserted as a string
-		// fmt.Println(id, organisation_id)
 		fmt.Println("i =>", i)
 		fmt.Println("value =>", val)
 
@@ -60,6 +55,5 @@ func Fetch() {
 		fmt.Println("Type =>", val.Type)
 		fmt.Println("Version =>", val.Version)
 	}
-
-	// return accounts
+	
 }
