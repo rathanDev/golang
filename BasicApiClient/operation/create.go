0218ee521eed9d5841533a3259c7412994a8d40c
jana <rathan.kparam@gmail.com>
2022-07-19 07:40:35 +0800

    Refactor create operation

--- BasicApiClient/operation/create.go
+++ BasicApiClient/operation/create.go
@@ -9,34 +9,20 @@ import (
 )
 
 func Create() {
-
 	url := config.AccountUrl()
 
-	var jsonPayload = []byte(`{
-		"data": {
-		  "type": "accounts",
-		  "id": "ad27e265-9605-4b4b-a0e5-3003ea9cc504",
-		  "organisation_id": "eb0bd6f5-c3f5-44b2-b677-acd23cdde604",
-		  "attributes": {
-			"country": "GB",
-			"base_currency": "GBP",
-			"bank_id": "400300",
-			"bank_id_code": "GBDSC",
-			"bic": "NWBKGB22",
-			"name": [
-			  "Janarthan"
-			],
-			"alternative_names": [
-			  "Jana"
-			],
-			"account_classification": "Personal",
-			"joint_account": false,
-			"account_matching_opt_out": false,
-			"secondary_identification": "A1B2C3D4"
-		  }
-		}
-	  }
-	`)
+	var accountType = "accounts"
+	var id = "eb0bd6f5-c3f5-44b2-b677-acd23cdde509"
+	var orgId = "eb0bd6f5-c3f5-44b2-b677-acd23cdde609"
+	var country = "SG"
+	var baseCurrency = "SGD"
+	var bankId = "400300"
+	var bankIdCode = "GBDSC"
+	var bic = "NWBKGB22"
+
+	var jsonStr = createPayload(accountType, id, orgId, country, baseCurrency, bankId, bankIdCode, bic)
+	var jsonPayload = []byte(jsonStr)
+
 	resp, err := http.Post(url, "application/json", bytes.NewBuffer(jsonPayload))
 	if err != nil {
 		log.Fatal(err)
@@ -45,6 +31,39 @@ func Create() {
 	fmt.Println("response:", resp)
 	fmt.Println("response Status:", resp.Status)
 	fmt.Println("response Headers:", resp.Header)
+}
+
+func createPayload(accountType string, id string, orgId string, country string, baseCurrency string, bankId string, bankIdCode string, bic string) string {
+	const (
+		jsonTemplate = `{
+			"data": {
+			  "type": "%s",
+			  "id": "%s",
+			  "organisation_id": "%s",
+			  "attributes": {
+				"country": "%s",
+				"base_currency": "%s",
+				"bank_id": "%s",
+				"bank_id_code": "%s",
+				"bic": "%s",
+				"name": [
+				  "Janarthan"
+				],
+				"alternative_names": [
+				  "Jana"
+				],
+				"account_classification": "Personal",
+				"joint_account": false,
+				"account_matching_opt_out": false,
+				"secondary_identification": "A1B2C3D4"
+			  }
+			}
+		  }
+		`
+	)
+	var jsonStr = fmt.Sprintf(jsonTemplate, accountType, id, orgId, country, baseCurrency, bankId, bankIdCode, bic)
+	// fmt.Println(jsonStr)
+	return jsonStr
 }
 
 func createNumberPointer(x int64) *int64 {
