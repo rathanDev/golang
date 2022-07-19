78c51c5e91e6312c0ef76d9762aa22d4c14a2de7
jana <rathan.kparam@gmail.com>
2022-07-19 21:03:01 +0800

    Refactor

--- BasicApiClient/main.go
+++ BasicApiClient/main.go
@@ -1,17 +1,42 @@
 package main
 
 import (
+	"api-client/model"
 	"api-client/operation"
 	"fmt"
 )
 
 func main() {
 
-	// tryFetch()
+	testCreate()
 
 	// operation.Create()
 
-	operation.Delete()
+	// operation.Delete()
+}
+
+func testCreate() {
+	var accountData model.AccountData
+	accountData.ID = "eb0bd6f5-c3f5-44b2-b677-acd23cdde515"
+	accountData.OrganisationID = "eb0bd6f5-c3f5-44b2-b677-acd23cdde615"
+	accountData.Type = "accounts"
+	accountData.Version = operation.CreateNumberPointer(1)
+
+	var accountAttr model.AccountAttributes
+	accountAttr.AccountClassification = operation.CreateStringPointer("Personal")
+	accountAttr.AccountMatchingOptOut = operation.CreateBooleanPointer(false)
+	accountAttr.AccountNumber = "1005"
+	accountAttr.AlternativeNames = []string{"Jana", "Rathan"}
+	accountAttr.BankID = "400300"
+	accountAttr.BankIDCode = "GBDSC"
+	accountAttr.BaseCurrency = "SGD"
+	accountAttr.Bic = "NWBKGB22"
+	accountAttr.Country = operation.CreateStringPointer("SG")
+	accountAttr.Name = []string{"Jana", "Param"}
+
+	accountData.Attributes = &accountAttr
+
+	operation.Create(accountData)
 }
 
 func tryFetch() {
--- BasicApiClient/operation/create.go
+++ BasicApiClient/operation/create.go
@@ -10,42 +10,20 @@ import (
 	"net/http"
 )
 
-func Create() {
+func Create(accountData model.AccountData) {
 	url := config.AccountUrl()
 
-	var accountData model.AccountData
-	accountData.ID = "eb0bd6f5-c3f5-44b2-b677-acd23cdde514"
-	accountData.OrganisationID = "eb0bd6f5-c3f5-44b2-b677-acd23cdde614"
-	accountData.Type = "accounts"
-	accountData.Version = createNumberPointer(1)
-
-	var accountAttr model.AccountAttributes
-	accountAttr.AccountClassification = createStringPointer("Personal")
-	accountAttr.AccountMatchingOptOut = createBooleanPointer(false)
-	accountAttr.AccountNumber = "1004"
-	accountAttr.AlternativeNames = []string{"Jana", "Rathan"}
-	accountAttr.BankID = "400300"
-	accountAttr.BankIDCode = "GBDSC"
-	accountAttr.BaseCurrency = "SGD"
-	accountAttr.Bic = "NWBKGB22"
-	accountAttr.Country = createStringPointer("SG")
-	accountAttr.Name = []string{"Jana", "Param"}
-
-	accountData.Attributes = &accountAttr
-
-	payload := createPayloadV(accountData)
+	payload := createPayload(accountData)
 
 	resp, err := http.Post(url, "application/json", bytes.NewBuffer(payload))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println("response:", resp)
-	fmt.Println("response Status:", resp.Status)
-	fmt.Println("response Headers:", resp.Header)
+	printResponse(resp)
 }
 
-func createPayloadV(accountData model.AccountData) []byte {
+func createPayload(accountData model.AccountData) []byte {
 	a, _ := json.Marshal(accountData)
 	fmt.Println(string(a))
 	const (
@@ -59,14 +37,21 @@ func createPayloadV(accountData model.AccountData) []byte {
 	return payload
 }
 
-func createBooleanPointer(x bool) *bool {
+func CreateBooleanPointer(x bool) *bool {
 	return &x
 }
 
-func createStringPointer(x string) *string {
+func CreateStringPointer(x string) *string {
 	return &x
 }
 
-func createNumberPointer(x int64) *int64 {
+func CreateNumberPointer(x int64) *int64 {
 	return &x
+}
+
+func printResponse(resp *http.Response) {
+	fmt.Println("response:", resp)
+	fmt.Println("response:", resp)
+	fmt.Println("response Status:", resp.Status)
+	fmt.Println("response Headers:", resp.Header)
 }
