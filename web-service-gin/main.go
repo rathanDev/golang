446f63891947fc50e6eaf963abc4e63d419bf200
jana <rathan.kparam@gmail.com>
2023-04-23 21:58:24 +0800

    Refactor

--- web-service-gin/main.go
+++ web-service-gin/main.go
@@ -14,23 +14,6 @@ import (
 	"log"
 )
 
-type album struct {
-	ID     string  `json:"id"`
-	Title  string  `json:"title"`
-	Artist string  `json:"artist"`
-	Price  float64 `json:"price"`
-}
-
-var albums = []album{
-	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
-}
-
-func getAlbums(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, albums)
-}
-
 type RegisterRequest struct {
 	NRIC          string `json:"nric"`
 	WalletAddress string `json:"walletAddress"`
@@ -127,12 +110,6 @@ func isWalletUnique(walletAddress string) bool {
 	return count == 0
 }
 
-type City struct {
-    Id         int
-    Name       string
-    Population int
-}
-
 func initDb() {
 	connection, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/testdb")
 	if err != nil {
@@ -143,28 +120,8 @@ func initDb() {
 }
 
 func main() {
-
 	initDb()
-
-    res, err := db.Query("SELECT * FROM city")
-	defer res.Close()
-	if err != nil {
-        log.Fatal(err)
-    }
-
-	for res.Next() {
-        var city City
-        err := res.Scan(&city.Id, &city.Name, &city.Population)
-        if err != nil {
-            log.Fatal(err)
-        }
-        fmt.Printf("%v\n", city)
-    }
-	// defer db.Close()
-
 	router := gin.Default()
-	router.GET("/albums", getAlbums)
-
 	router.POST("/register", handleRegister)
 	router.Run("localhost:8080")
 }
