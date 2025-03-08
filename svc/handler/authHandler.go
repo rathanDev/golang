b3d3f51cfa7334daf31e255c6907a6e3e305a34f
Jana <rathan.kparam@gmail.com>
2025-03-08 08:32:13 +0800

    No smart developer can fail

--- svc/handler/authHandler.go
+++ svc/handler/authHandler.go
@@ -12,6 +12,20 @@ func SetRoutes(route *gin.Engine) {
 	route.POST("/login", service.Login)
 
 	authorized := route.Group("/")
-	authorized.Use(middleware.TokenAuthMiddleware())
-	authorized.GET("/users", service.GetUsers)
+	{
+		// authorized.Use(middleware.TokenAuthMiddleware())
+		// authorized.GET("/users", service.GetUsers)
+
+		// Admin-only route
+		authorized.GET("/admin", middleware.TokenAuthMiddleware(secretKey, []constant.Role{constant.ADMIN}), handlers.AdminHandler)
+
+		// Doctor and Admin route
+		auth.GET("/doctor", middleware.AuthMiddleware(secretKey, []constant.Role{constant.DOC, constant.ADMIN}), handlers.DoctorHandler)
+
+		// Patient and Admin route
+		auth.GET("/patient", middleware.AuthMiddleware(secretKey, []constant.Role{constant.PAT, constant.ADMIN}), handlers.PatientHandler)
+
+
+	}
+
 }
